c31501ce7c6d3bef60bc7caa63bfea51f5bf05a0
ASUS <omeremreu361@gmail.com>
2025-02-14 18:05:12 +0300

    Kullanıcı adı oluşturuldu

--- kullaniciUyg/src/main/java/org/example/kullaniciuyg.java
+++ kullaniciUyg/src/main/java/org/example/kullaniciuyg.java
@@ -6,14 +6,28 @@ import java.awt.*;
 public class kullaniciuyg {
 
 
-    JFrame frame = new JFrame();
+    JFrame frame;
+    JPanel panelust;
+    JTextField kullaniciadi;
 
     public kullaniciuyg()
     {
+        panelust = new JPanel();
+        panelust.setLayout(new FlowLayout());
+        JLabel labelust = new JLabel();
+
+        kullaniciadi = new JTextField();
+        kullaniciadi.setPreferredSize(new Dimension(200,40));
+
+        panelust.add(labelust);
+        panelust.add(kullaniciadi);
+
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,300);
         frame.setLayout(new GridLayout(3,2));
+
+        frame.add(panelust);
         frame.setVisible(true);
     }
 }
