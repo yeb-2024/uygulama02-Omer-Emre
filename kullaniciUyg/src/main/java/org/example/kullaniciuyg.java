6653ef6b1e2e94a97a363d49f3686a237a517814
ASUS <omeremreu361@gmail.com>
2025-02-14 18:40:39 +0300

    Kullanıcı adı ve şifre oluşturuldu boşuk sorunu çözüldü

--- kullaniciUyg/src/main/java/org/example/kullaniciuyg.java
+++ kullaniciUyg/src/main/java/org/example/kullaniciuyg.java
@@ -2,25 +2,44 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class kullaniciuyg {
+public class kullaniciuyg implements ActionListener {
 
 
     JFrame frame;
     JPanel panelust;
+    JPanel panelalt;
+    JButton buton;
     JTextField kullaniciadi;
+    JTextField şifre;
 
     public kullaniciuyg()
     {
         panelust = new JPanel();
         panelust.setLayout(new FlowLayout());
-        JLabel labelust = new JLabel();
+        JLabel labelust = new JLabel("Kullanıcı Adı :");
 
         kullaniciadi = new JTextField();
-        kullaniciadi.setPreferredSize(new Dimension(200,40));
+        kullaniciadi.setPreferredSize(new Dimension(300,40));
+
+        şifre = new JTextField();
+        şifre.setPreferredSize(new Dimension(300,40));
+
+        buton = new JButton("Giriş Yap");
+        buton.addActionListener(this);
+
+        JLabel labelalt = new JLabel("Şifre :");
+
+        panelalt = new JPanel();
+        panelalt.setPreferredSize(new Dimension(300,40));
 
         panelust.add(labelust);
         panelust.add(kullaniciadi);
+
+        panelalt.add(labelalt);
+        panelalt.add(şifre);
 
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,6 +47,22 @@ public class kullaniciuyg {
         frame.setLayout(new GridLayout(3,2));
 
         frame.add(panelust);
+        frame.add(panelalt);
+        frame.add(buton);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == buton)
+        {
+           String kullaniciadi = this.kullaniciadi.getText().trim();
+            String şifre = this.şifre.getText().trim();
+
+            if(kullaniciadi.isEmpty() || şifre.isEmpty())
+            {
+                JOptionPane.showMessageDialog(null,"Lütten tüm alanları doldurun", "Hata",JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 }
